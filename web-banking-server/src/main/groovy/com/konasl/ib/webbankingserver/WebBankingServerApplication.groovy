487b3cdff896da924038cef4a81926408694372f
Muhammad Shamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2020-10-02 05:29:18 +0000

    Add new groovy spring boot server module
    Spring boot with groovy added as server module

---
+++ web-banking-server/src/main/groovy/com/konasl/ib/webbankingserver/WebBankingServerApplication.groovy
@@ -0,0 +1,13 @@
+package com.konasl.ib.webbankingserver
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WebBankingServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(WebBankingServerApplication, args)
+	}
+
+}
---
+++ web-banking-server/src/test/groovy/com/konasl/ib/webbankingserver/WebBankingServerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.konasl.ib.webbankingserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebBankingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
